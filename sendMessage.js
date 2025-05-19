b4225f7229f64a25aaa465e7013d451134833a6c
TakioN <jungbeen0724@gmail.com>
2025-05-20 04:06:58 +0900

    refactor: update rabbitmq code into await/async

--- sendMessage.js
+++ sendMessage.js
@@ -1,6 +1,5 @@
 const { getLeastLoadedQueue } = require("./getLeastQueue");
-// const amqp = require("amqplib/callback_api");
-const amqp = require("amqplib/callback-api");
+const amqp = require("amqplib");
 const { v4: uuidv4 } = require("uuid");
 
 // 메세지 발행
@@ -24,66 +23,30 @@ async function sendMessage(fileUrl, inputData, mode) {
   try {
     // const queue = await getLeastLoadedQueue();
 
-    amqp.connect(
-      `amqp://${process.env.RABBITMQ_ID}:${process.env.RABBITMQ_PWD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
-      (err, conn) => {
-        if (err) {
-          console.error("AMQP Connection error:", err);
-          return;
-        }
-
-        conn.createChannel(async (err, ch) => {
-          if (err) {
-            console.error("[AMQP] Channel creation error:", err);
-            return;
-          }
-
-          const uuid = uuidv4();
-          let queue = await getLeastLoadedQueue("s");
-          if (queue === null) {
-            queue = mode === "c" ? "master1.compute_req" : "master1.save_req";
-          }
-          const exchange = "req_exchange";
-          const routingKey = queue;
-          const res_routingKey = `router.${uuid}`;
-          const msg = JSON.stringify({ fileUrl, res_routingKey });
-
-          console.log(queue);
-
-          // ch.assertExchange(exchange, "direct", { durable: true });
-
-          ch.assertExchange(
-            exchange,
-            "direct",
-            { durable: true },
-            (err, ok) => {
-              if (err) {
-                console.error("Exchange assert error:", err);
-                return;
-              }
-
-              ch.assertQueue(queue, { durable: true }, (err, ok) => {
-                ch.bindQueue(queue, exchange, routingKey, {}, () => {
-                  publishMsg(ch, exchange, routingKey, msg);
-                  consumeMsg();
-                });
-              });
-            }
-          );
-
-          //   const queue = "save_req";
-
-          //   ch.assertQueue(queue, { durable: true });
-          //   ch.sendToQueue(queue, Buffer.from(msg), { persistent: true });
-
-          //   console.log("[SEND] File URL and Request ID sent to queue:", msg);
-        });
-
-        setTimeout(() => {
-          conn.close();
-        }, 500);
-      }
+    const connection = await amqp.connect(
+      `amqp://${process.env.RABBITMQ_ID}:${process.env.RABBITMQ_PWD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`
     );
+    const channel = await connection.createChannel();
+
+    const uuid = uuidv4();
+    let queue = await getLeastLoadedQueue("s");
+    if (queue === null) {
+      queue = mode === "c" ? "master1.compute_req" : "master1.save_req";
+    }
+    const exchange = "req_exchange";
+    const routingKey = queue;
+    const res_routingKey = `router.${uuid}`;
+    const msg = JSON.stringify({ fileUrl, res_routingKey });
+
+    await channel.assertExchange(exchange, "direct", { durable: true });
+    channel.publish(exchange, routingKey, Buffer.from(msg), {
+      persistent: true,
+    });
+
+    setTimeout(async () => {
+      await channel.close();
+      await connection.close();
+    }, 500);
   } catch (e) {
     console.error(e);
   }
